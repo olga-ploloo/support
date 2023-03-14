aac3eb7a95987fce8e845516976a01cc696c8500
olga-ploloo <olga.kozintsava@gmail.com>
2023-03-14 17:54:50 +0300

    updated user registration and added tests

--- tests/support/test_user.py
+++ tests/support/test_user.py
@@ -1,6 +1,19 @@
 import pytest
+from rest_framework.test import APIClient
+
+client = APIClient()
 
 
 @pytest.mark.django_db
 def test_register_user():
-    pass
+    payload = dict(
+        username='testusername',
+        email='testemail1@test.com',
+        password='testpassword',
+        password2='testpassword'
+    )
+    response = client.post('/register/', payload)
+
+    assert response.data['username'] == payload['username']
+    assert response.data['email'] == payload['email']
+    assert 'password' not in response.data
