7164f4511c3d291d5822e5051e3701a72f51cf47
olga-ploloo <olga.kozintsava@gmail.com>
2023-05-19 10:14:40 +0300

    feat front: added unassigned card

---
+++ frontend/src/components/unassignedTicketsList.js
@@ -0,0 +1,60 @@
+import React, {useEffect, useState} from "react";
+import axios from "axios";
+import * as constants from "../constatns/ticketConstans";
+import {Card, CardBody, CardTitle, ListGroup, ListGroupItem, Table} from "reactstrap";
+import moment from "moment/moment";
+import {Link} from "react-router-dom";
+
+
+const UnassignedTicketsList = () => {
+    const [tickets, setTickets] = useState([]);
+    const getUnassignedTickets = async () => {
+        try {
+            const response = await axios.get(`${constants.API_URL}/tickets/unassigned_tickets`);
+            setTickets(response.data.results);
+            console.log(response)
+        } catch (error) {
+            console.log(error)
+        }
+    };
+
+    useEffect(() => {
+        getUnassignedTickets();
+    }, [])
+
+    return (
+        <>
+            <Card className="home-card"
+                  color="danger"
+                  outline>
+                <CardBody>
+                    <CardTitle tag="h5">
+                        Unassigned tickets:
+                    </CardTitle>
+                </CardBody>
+                <ListGroup flush>
+                    {!tickets || tickets.length <= 0 ? (
+                        <ListGroupItem>
+                            No unassigned tickets.
+                        </ListGroupItem>
+                    ) : (
+                        tickets.slice(0, 5).map((ticket, index) => (
+                            <Link to={`/tickets/${ticket.id}/`} className="card-btn-link">
+                                <ListGroupItem>
+                                    № {ticket.id} {moment(ticket.created_at).format('DD/MM/YYYY')}: {ticket.status}
+                                </ListGroupItem>
+                            </Link>
+
+                        ))
+                    )}
+                </ListGroup>
+
+
+            </Card>
+
+        </>
+    )
+
+}
+
+export default UnassignedTicketsList;
