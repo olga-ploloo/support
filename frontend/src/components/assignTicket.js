ed7c4b7bfe4650bb7bf77755551b52cf3bd96136
olga-ploloo <olga.kozintsava@gmail.com>
2023-06-02 15:22:23 +0300

    refactor: the AssignTicket component returns the updated ticket data

--- frontend/src/components/assignTicket.js
+++ frontend/src/components/assignTicket.js
@@ -1,15 +1,13 @@
-import React, {useEffect, useState} from "react";
+import React from "react";
 import axios from "axios";
 import * as constants from "../constatns/ticketConstans";
-import {Card, CardBody, CardTitle, ListGroup, ListGroupItem} from "reactstrap";
-import {Link} from "react-router-dom";
-import moment from "moment";
+
 
 const AssignTicket = ({assignTicketId, update}) => {
     const assignTicket = async (assignTicketId) => {
         try {
             const response = await axios.put(`${constants.API_URL}/assign_ticket/${assignTicketId}/`);
-            update();
+            update(response.data);
         } catch (error) {
             console.error(error);
         }
